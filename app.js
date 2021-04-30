a284631bed8d199bb0f2c073a1f1f2fc0de85601
Malik-Umair-khalid <81845666+Malik-Umair-khalid@users.noreply.github.com>
2021-04-30 07:32:38 -0700

    Not Completed

---
+++ app.js
@@ -0,0 +1,59 @@
+// // *** Q : 1 ***
+// var multiarray = [["","",""]["","",""]]
+
+
+// // *** Q : 2 ***
+// var multiarray = [["2","3","4"]]
+// document.write(multiarray)
+
+
+// // *** Q : 3 ***
+// for(var i = 0; i <= 10; i++){
+//  document.write(i + '<br/>')
+// }
+
+
+// // *** Q : 4 ***
+// var table = +prompt("Which Table Do You Want To Print")
+// var length = +prompt("How many Times Yow Want The Table")
+//  for(var i = 1; i <= length; i++){
+//      document.write(table , "X" , i , "=" , table*i , '<br/>')
+//  }
+
+// // *** Q : 5 ***
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"]
+// for(var  i = 0; i < fruits.length; i++){
+//   document.write(fruits[i] + "<br/>")
+// }
+
+// *** Q : 6 ***
+
+
+
+
+// // *** Q : 7 ***
+
+// var writeFood = prompt("Write The Name OF Your Food")
+// var foods = ["cake", "apple pie", "cookie", "chips", "patties"]
+// for(var i = 0; i <= foods.length; i++){
+// if(writeFood === foods[i]){
+//     alert("The Food IS In The List")
+//     break;
+// }
+// else{
+//     alert("The Food Is Not In The List")
+//     break;
+// }
+// }
+
+// *** Q : 6 ***
+
+
+
+
+// *** Q : 6 ***
+
+
+
+
+// *** Q : 6 ***
